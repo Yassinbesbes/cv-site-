45e9045746e8ce1ec4cfad1a034f4e730b01164f
yassin besbes <besbesyassin90@gmail.com>
2024-09-18 22:50:12 +0100

    WIP: Refactor MyTestimonials component layout and add customer details

--- cvapp/src/scenes/HomePage.jsx
+++ cvapp/src/scenes/HomePage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Description from "../components/Description/Description.js";
 import About from "../components/About/index.tsx";
 import MyWork from "../components/MyWork/index.tsx";
-
+import MyTestimonials from '../components/MyTestimonials/index.tsx'
 function HomePage() {
   return (
     <>
       <Description />
       <About />
       <MyWork />
+      <MyTestimonials />
     </>
   );
 }
